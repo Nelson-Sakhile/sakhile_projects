ad7ad4fe13de5417a240a8e2838b604c497d7750
Nelson-Sakhile <53044763+Nelson-Sakhile@users.noreply.github.com>
2022-07-12 00:57:44 +0200

    static collect

--- brixton_drives_site/settings.py
+++ brixton_drives_site/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +131,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticfilesStorage'
+
 # Email settings
 
 EMAIL_HOST = 'localhost'
@@ -137,4 +142,4 @@ EMAIL_HOST_PASSWORD =  ''
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = False
 
-
+django_heroku.settings(locals())
